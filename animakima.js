62c484455777ac3fe664e013779175bd59a51c81
BaBaYkQ <babayk0.q1@gmail.com>
2026-04-23 13:24:16 +0300

    Refactor Animakima plugin initialization
    Removed the startPlugin function and its invocation.

--- animakima.js
+++ animakima.js
@@ -1,19 +1,4 @@
 (function () {
     'use strict';
-
-    function startPlugin() {
-        console.log('Animakima plugin started');
-
-        if (!Lampa.Storage.get('animakima_source_added')) {
-            Lampa.Storage.set('animakima_source_added', true);
-        }
-    }
-
-    if (window.appready) {
-        startPlugin();
-    } else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') startPlugin();
-        });
-    }
+    console.log('Animakima loaded');
 })();

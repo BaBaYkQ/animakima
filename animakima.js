2d9cf59a4cf99528d74b57745b43a3e97a0a484f
BaBaYkQ <babayk0.q1@gmail.com>
2026-04-23 13:32:00 +0300

    Add Animakima source plugin initialization

--- animakima.js
+++ animakima.js
@@ -1,4 +1,33 @@
 (function () {
     'use strict';
-    console.log('Animakima loaded');
+
+    function startPlugin() {
+        console.log('Animakima source plugin started');
+
+        if (!Lampa.Params) return;
+
+        if (!Lampa.Params.values.source) {
+            Lampa.Params.values.source = [];
+        }
+
+        let exists = Lampa.Params.values.source.some(function(s){
+            return s.value === 'animakima';
+        });
+
+        if (!exists) {
+            Lampa.Params.values.source.push({
+                title: 'Animakima',
+                value: 'animakima'
+            });
+        }
+
+        console.log('Animakima source registered');
+    }
+
+    if (window.appready) startPlugin();
+    else {
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type === 'ready') startPlugin();
+        });
+    }
 })();
